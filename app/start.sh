ad37522dc4ba97a72780bfe5d6ea69bddbe453c1
Drew Simon <drew.simon@gmail.com>
2020-02-05 17:05:17 -0600

    remove debug and run start command

--- app/start.sh
+++ app/start.sh
@@ -18,8 +18,5 @@ if [ ! -c /dev/fb1 ] && [ "$TFT" = "1" ]; then
   mknod /dev/fb1 c $(cat /sys/class/graphics/fb1/dev | tr ':' ' ') || true
   FRAMEBUFFER=/dev/fb1 startx /usr/src/app/node_modules/electron/dist/electron /usr/src/app --enable-logging
 else
-  while true; do
-    sleep 5
-  done
-  #startx /usr/src/app/node_modules/electron/dist/electron /usr/src/app --enable-logging
+  startx /usr/src/app/node_modules/electron/dist/electron /usr/src/app --enable-logging
 fi
